8f8a3f53eaa3961740f7f8451157522427d15ca1
Joseph Arias <arias.joseph.a@gmail.com>
2021-10-08 01:23:37 -0400

    implemented lifecycle hooks

--- src/app/form-input/form-input.component.ts
+++ src/app/form-input/form-input.component.ts
@@ -18,6 +18,7 @@ export class FormInputComponent implements OnInit {
 
   constructor() { }
 
+  // life cycle hook
   ngOnInit(): void {
   }
 
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,17 +1,63 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { 
+  Component, 
+  OnInit, 
+  Input, 
+  OnChanges, 
+  SimpleChanges, 
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy{
   // adding a decorator to allow parent component to bind to this property
-  @Input() element: {type: string, name: string, content: string}
+  @Input() element: {type: string, name: string, content: string};
 
-  constructor() { }
+  @Input() name: string;
+
+  constructor() {
+    console.log('constructor called');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called');
+    console.log(changes);
+  }
 
   ngOnInit(): void {
+    console.log('ngOnInit called');
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck called')
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit called');
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked called');
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit called');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked called');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestory called');
   }
 
 }
